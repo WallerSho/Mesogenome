7471c40c53b0537c9d863f8c832454043b44db73
Sho Waller <shohei.waller@gmail.com>
2024-05-29 16:23:17 +1000

    added a few more maf files, ready to try mergeMAF script

--- merge mafs.R
+++ merge mafs.R
@@ -1,5 +1,8 @@
+##Load required libraries
+
 library(maftools)
 library(tidyverse)
+library(optparse)
 
 ## Test using single maf file
 
@@ -11,3 +14,23 @@ maf_SBJ04866 <- read.maf(maf = "/Users/Shohei/Desktop/mesogenome maf files to me
 summary(maf_SBJ04866)
 
 oncoplot(maf=maf_SBJ04866)
+
+## Add a few more maf files to environment
+
+file_path_SBJ04777 <- file.choose()
+print(file_path_SBJ04777)
+maf_SBJ04777 <- read.maf(maf = "/Users/Shohei/Desktop/mesogenome maf files to merge/SBJ04777__MDX240043-somatic-PASS.maf")
+
+file_path_SBJ04776 <- file.choose()
+print(file_path_SBJ04776)
+maf_SBJ04776 <- read.maf(maf = "/Users/Shohei/Desktop/mesogenome maf files to merge/SBJ04776__MDX240053-somatic-PASS.maf")
+
+maf_files_to_merge <- c(maf_SBJ04866, maf_SBJ04777, maf_SBJ04776)
+maf_files_to_merge
+
+## Try to use mergeMAF script
+
+
+
+## Try mergeMAF script for above three files
+
